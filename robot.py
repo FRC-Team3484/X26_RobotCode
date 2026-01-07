1a7801556a04eb9d90d8f0e8636f62c418b85e6d
ShadeCoder707 <aidanclark5835@gmail.com>
2026-01-06 20:16:27 -0500

    Initial Setup

---
+++ robot.py
@@ -0,0 +1,42 @@
+import commands2 
+
+
+class MyRobot(commands2.TimedCommandRobot):
+    def __init__(self):
+        pass
+
+    def robotInit(self):
+        pass
+
+    def robotPeriodic(self):
+        pass
+
+    def autonomousPeriodic(self):
+        pass
+
+    def autonomousInit(self):
+        pass
+
+    def autonomousExit(self):
+        pass
+
+    def teleopInit(self):
+        pass
+
+    def teleopPeriodic(self):
+        pass
+
+    def teleopExit(self):
+        pass
+
+    def testInit(self):
+        pass
+
+    def testPeriodic(self):
+        pass
+
+    def testExit(self):
+        pass
+
+
+
