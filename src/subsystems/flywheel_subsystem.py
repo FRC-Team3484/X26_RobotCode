67360e9fdf6210d450c6ee6a6e0f3bf7ca7b9c1c
Ethan Francis <ethanfrancis577@gmail.com>
2026-01-19 19:57:02 -0500

    feat: added comments and flywheel subsystem

---
+++ src/subsystems/flywheel_subsystem.py
@@ -0,0 +1,60 @@
+from frc3484 import VelocityMotor, SC_LauncherSpeed
+from constants import FlywheelSubsystemConstants
+from commands2 import Subsystem
+
+from wpimath.units import revolutions_per_minute
+
+class FlywheelSubsystem(Subsystem):
+    def __init__(self) -> None:
+        
+        super().__init__()
+        
+        self._motor: VelocityMotor = VelocityMotor(
+            FlywheelSubsystemConstants.motor_config,
+            FlywheelSubsystemConstants.pid_config,
+            FlywheelSubsystemConstants.feed_forward_config,
+            FlywheelSubsystemConstants.gear_ratio,
+            FlywheelSubsystemConstants.tolerance
+        )
+
+    def periodic(self) -> None:
+        pass
+
+    def set_speed(self, speed: SC_LauncherSpeed) -> None:
+        '''
+        Sets the motor speed
+
+        Parameters:
+            - speed (SC_LauncherSpeed): the speed and power to set the motor to
+        '''
+        self._motor.set_speed(speed)
+
+    def is_at_speed(self) -> bool:
+        '''
+        Checks whether the motor is at the target speed
+
+        Returns:
+            - bool: `True` if the motor is at the target speed, `False` otherwise
+        '''
+        return self._motor.at_target_speed()
+
+
+
+r'''
+this is a very important comment dont delete
+      ________
+     /       /\       ______ cube™ :OO
+    /       /..\     /
+   /_______/....\ <-/
+   \#######\..../ 
+    \#######\../ 
+     \#######\/
+
+(c) SOUPOFFICE.AI™: always and forever
+     
+'''
+
+
+r'''
+(c) SOUPOFFICE.AI™: always and forever
+'''
