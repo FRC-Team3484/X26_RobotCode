f067300141a66e6b3519c63b65edd27a3e27e962
Ethan Francis <ethanfrancis577@gmail.com>
2026-01-22 19:52:19 -0500

    feet: bound inputs for testing and demo controller

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 from frc3484.controls import XboxControllerMap, Input
 # Constants
+controller = XboxControllerMap
 class UserInterface:
     class TestConstants1:
         CONTROLLER_PORT: int = 2
@@ -16,6 +17,12 @@ class UserInterface:
         QUASI_REV_BUTTON: Input = XboxControllerMap.B_BUTTON
         DYNAMIC_FWD_BUTTON: Input = XboxControllerMap.X_BUTTON
         DYNAMIC_REV_BUTTON: Input = XboxControllerMap.Y_BUTTON
+        
+        FLYWHEEL_INPUT: Input = controller.RIGHT_TRIGGER
+        SPINDEXER_INPUT: Input = controller.LEFT_TRIGGER
+        TURRET_INPUT: Input = controller.LEFT_JOY_X
+        CLIMBER_INPUT: Input = controller.RIGHT_JOY_X
+    
     class TestConstants2:
         CONTROLLER_PORT: int = 2
         JOYSTICK_DEADBAND: float = 0.02
@@ -27,7 +34,36 @@ class UserInterface:
         RUMBLE_LOW: float = 0.2
         RUMBLE_OFF: float = 0.0
 
-        FLYWHEEL_INPUT: Input = XboxControllerMap.RIGHT_TRIGGER
+        INTAKE_ROLLER_INPUT: Input = controller.RIGHT_TRIGGER
+        FEEDER_INPUT: Input = controller.LEFT_JOY_Y
+        INTAKE_PIVOT_INPUT: Input = controller.RIGHT_JOY_Y
+    
+    class DemoController:
+        CONTROLLER_PORT: int = 2
+        JOYSTICK_DEADBAND: float = 0.02
+
+        AXIS_LIMIT: float = 0.5 # How far an axis must move to be considered "pressed"
+        TRIGGER_LIMIT: float = 0.5 # How far a trigger must be pressed to be considered "pressed"
+
+        RUMBLE_HIGH: float = 0.5
+        RUMBLE_LOW: float = 0.2
+        RUMBLE_OFF: float = 0.0
+
+        FLYWHEEL_LEFT_INPUT: Input = controller.LEFT_TRIGGER
+        FLYWHEEL_RIGHT_INPUT: Input = controller.RIGHT_TRIGGER
+
+        TURRET_INPUT: Input = controller.DPAD_X
+        FEED_INPUT: Input = controller.A_BUTTON
+        EJECT_INPUT: Input = controller.B_BUTTON
+        INTAKE_INPUT: Input = controller.X_BUTTON
+        
+        THROTTLE_INPUT: Input = controller.LEFT_JOY_Y
+        STRAFE_INPUT: Input = controller.LEFT_JOY_X
+        ROTATE_INPUT: Input = controller.RIGHT_JOY_X
+
+
+
+
         
 # Subsystems
 class AgitatorSubsystemConstants:
--- src/oi.py
+++ src/oi.py
@@ -3,6 +3,7 @@ from constants import UserInterface
 
 _TEST_INPUTS1: type[UserInterface.TestConstants1] = UserInterface.TestConstants1
 _TEST_INPUTS2: type[UserInterface.TestConstants2] = UserInterface.TestConstants2
+_DEMO_INPUTS: type[UserInterface.DemoController] = UserInterface.DemoController
 
 class TestInterface:
     _controller1: SC_Controller = SC_Controller(
@@ -18,7 +19,67 @@ class TestInterface:
         _TEST_INPUTS2.JOYSTICK_DEADBAND
     )
 
-    def get_wheel_power(self) -> float:
-        return self._controller2.get_axis(_TEST_INPUTS2.FLYWHEEL_INPUT)
+    _demo_controller: SC_Controller = SC_Controller(
+        _DEMO_INPUTS.CONTROLLER_PORT,
+        _DEMO_INPUTS.AXIS_LIMIT,
+        _DEMO_INPUTS.TRIGGER_LIMIT,
+        _DEMO_INPUTS.JOYSTICK_DEADBAND
+    )
 
-    # add indexer controller 2
+    ## CONTROLLER 1
+
+    def get_wheel_power(self) -> float:
+        return self._controller1.get_axis(_TEST_INPUTS1.FLYWHEEL_INPUT)
+
+    def get_spindexer(self) -> float:
+        return self._controller1.get_axis(_TEST_INPUTS1.SPINDEXER_INPUT)
+    
+    def get_turret(self) -> float:
+        return self._controller1.get_axis(_TEST_INPUTS1.TURRET_INPUT)
+
+    def get_climber(self) -> float:
+        return self._controller1.get_axis(_TEST_INPUTS1.CLIMBER_INPUT)
+
+    ## END CONTROLLER 1
+    ## CONTROLLER 2
+
+    def get_intake_roller(self) -> float:
+        return self._controller2.get_axis(_TEST_INPUTS2.INTAKE_ROLLER_INPUT)
+    
+    def get_intake_pivot(self) -> float:
+        return self._controller2.get_axis(_TEST_INPUTS2.INTAKE_PIVOT_INPUT)
+    
+    def get_feeder(self) -> float:
+        return self._controller2.get_axis(_TEST_INPUTS2.FEEDER_INPUT)
+    
+    ## END CONTROLLER 2
+    ## DEMO CONTROLLER
+
+    def _demo_get_flywheel(self) -> float:
+        return \
+        (self._demo_controller.get_axis(_DEMO_INPUTS.FLYWHEEL_LEFT_INPUT)*(1/3)) + \
+        (self._demo_controller.get_axis(_DEMO_INPUTS.FLYWHEEL_RIGHT_INPUT)*(2/3))
+    
+    def _demo_get_turret(self) -> float:
+        return self._demo_controller.get_axis(_DEMO_INPUTS.TURRET_INPUT)
+
+    def _demo_get_feed(self) -> bool:
+        return self._demo_controller.get_button(_DEMO_INPUTS.FEED_INPUT)
+
+    def _demo_get_eject(self) -> bool:
+        return self._demo_controller.get_button(_DEMO_INPUTS.EJECT_INPUT)
+    
+    def _demo_get_intake(self) -> bool:
+        return self._demo_controller.get_button(_DEMO_INPUTS.INTAKE_INPUT)
+    
+    def _demo_get_throttle(self) -> float:
+        return self._demo_controller.get_axis(_DEMO_INPUTS.THROTTLE_INPUT)
+    
+    def _demo_get_strafe(self) -> float:
+        return self._demo_controller.get_axis(_DEMO_INPUTS.STRAFE_INPUT)
+    
+    def _demo_get_rotate(self) -> float:
+        return self._demo_controller.get_axis(_DEMO_INPUTS.ROTATE_INPUT)
+
+
+    ## END DEMO CONTROLLER
