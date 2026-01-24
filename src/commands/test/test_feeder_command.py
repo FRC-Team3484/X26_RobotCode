a3c767ad8ee8c05655b3e6c32b91db4cd0fc7bc5
nfoert <nofoert@gmail.com>
2026-01-24 13:53:18 -0500

    feat(feeder): Create test comamnd for the feeder

---
+++ src/commands/test/test_feeder_command.py
@@ -0,0 +1,32 @@
+from typing import override
+from commands2 import Command
+
+from subsystems.feeder_subsystem import FeederSubsystem
+from oi import TestInterface
+
+class TestFeederCommand(Command):
+    """
+    The test command for the feeder
+
+    Based on joystick input, will set the power of the feeder
+
+    Parameters:
+        test_interface (TestInterface): the test interface
+        feeder_subsystem (FeederSubsystem): the feeder subsystem
+    """
+    def __init__(self, test_interface: TestInterface, feeder_subsystem: FeederSubsystem) -> None:
+        super().__init__()
+        self._test_interface: TestInterface = test_interface
+        self._feeder_subsystem: FeederSubsystem = feeder_subsystem
+
+    @override
+    def execute(self) -> None:
+        self._feeder_subsystem.set_power(self._test_interface.get_feeder())
+
+    @override
+    def end(self, interrupted: bool) -> None:
+        self._feeder_subsystem.set_power(0)
+
+    @override
+    def isFinished(self) -> bool:
+        return False
