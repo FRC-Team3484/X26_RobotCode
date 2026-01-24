516211b0c2bb216d157aa405b753f9482ba2afb9
nfoert <nofoert@gmail.com>
2026-01-24 14:29:28 -0500

    fix(intake): Improve styling of the intake test command

--- src/command/intake_test_command.py
+++ src/command/intake_test_command.py
@@ -1,24 +1,33 @@
+from typing import override
+
 from commands2 import Command
-from subsystems.intake_subsystem import IntakesubSystem
+
+from subsystems.intake_subsystem import IntakeSubsystem
 from oi import TestInterface
+
 class IntakeTestCommand(Command):
-    def __init__(self, intake: IntakesubSystem, oi: TestInterface):
+    """
+    Intake test command
+
+    Controls the intake based on controller inputs
+    """
+    def __init__(self, intake: IntakeSubsystem, oi: TestInterface):
         super().__init__()
-        self._intake = intake
-        self._oi = oi
+        self._intake: IntakeSubsystem = intake
+        self._oi: TestInterface = oi
         self.addRequirements(intake)
 
-    def initialize(self):
-        pass
-
+    @override
     def execute(self) -> None:
         self._intake.set_pivot_power(self._oi.get_intake_pivot())
         self._intake.set_roller_power(self._oi.get_intake_roller())
         self._intake.print_diagnostics()
 
+    @override
     def end(self, interrupted: bool) -> None:
         self._intake.set_pivot_power(0)
         self._intake.set_roller_power(0)
 
+    @override
     def isFinished(self) -> bool:
         return False
