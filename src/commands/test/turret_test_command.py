589f5b648ec17a93744508790f2ea736177c441b
nfoert <nofoert@gmail.com>
2026-01-27 18:34:21 -0500

    style: Add comments to the turret test command

--- src/commands/test/turret_test_command.py
+++ src/commands/test/turret_test_command.py
@@ -1,25 +1,38 @@
+from typing import override
 from commands2 import Command
 from subsystems.turret_subsystem import TurretSubsystem
 from oi import TestInterface
 
 class TurretTestCommand(Command):
-    def __init__(self, oi: TestInterface, turret_subsystem: TurretSubsystem):
+    """
+    A command for testing the turret subsystem
+
+    Based on joystick input, will set the angle of the turret
+
+    Parameters:
+        - oi (`oi.TestInterface`): the oi test interface for controller bindings
+        - turret_subsystem (`TurretSubsystem`): the turret subsystem
+    """
+    def __init__(self, oi: TestInterface, turret_subsystem: TurretSubsystem) -> None:
         super().__init__()
-        self._turret_subsystem = turret_subsystem
-        self._oi = oi
+        self._turret_subsystem: TurretSubsystem = turret_subsystem
+        self._oi: TestInterface = oi
         self.addRequirements(turret_subsystem)
 
-    def initialize(self):
+    @override
+    def initialize(self) -> None:
         pass
 
-    def execute(self):
+    @override
+    def execute(self) -> None:
         self._turret_subsystem.set_power(self._oi.get_turret())
         self._turret_subsystem.print_diagnostics()
     
-    def end(self, interrupted: bool):
-        
+    @override
+    def end(self, interrupted: bool) -> None:
         self._turret_subsystem.set_power(0)
 
+    @override
     def isFinished(self) -> bool:
         return False
 
