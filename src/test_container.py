1bba4a44c25da31ee905cd9293f997e1d707b370
nfoert <nofoert@gmail.com>
2026-01-28 19:46:49 -0500

    feat(robot-container): Add climber and indexer to the test container

--- src/test_container.py
+++ src/test_container.py
@@ -5,8 +5,10 @@ from wpilib import SendableChooser, SmartDashboard
 
 from oi import TestInterface
 from robot_container import RobotContainer
+from commands.test.climber_test_command import ClimberTestCommand
 from commands.test.flywheel_test_command import FlywheelTestCommand
 from commands.test.feeder_test_command import FeederTestCommand
+from commands.test.indexer_test_command import IndexerTestCommand
 from commands.test.intake_test_command import IntakeTestCommand
 from commands.test.turret_test_command import TurretTestCommand
 
@@ -34,8 +36,10 @@ class TestContainer:
         self._mode_chooser.addOption("Demo", Mode.DEMO)
         SmartDashboard.putData("Test Mode", self._mode_chooser)
 
+        SmartDashboard.putBoolean("Climber Enabled", False)
         SmartDashboard.putBoolean("Flywheel Test Enabled", False)
         SmartDashboard.putBoolean("Feeder Test Enabled", False)
+        SmartDashboard.putBoolean("Indexer Test Enabled", False)
         SmartDashboard.putBoolean("Intake Test Enabled", False)
         SmartDashboard.putBoolean("Turret Test Enabled", False)
 
@@ -57,11 +61,17 @@ class TestContainer:
         elif self._mode_chooser.getSelected() == Mode.MOTOR:
             commands: list[Command] = []
 
+            if SmartDashboard.getBoolean("Climber Enabled", False):
+                commands.append(ClimberTestCommand(self._robot_container.climber_subsystem, self._oi))
+
             if SmartDashboard.getBoolean("Flywheel Test Enabled", False):
                 commands.append(FlywheelTestCommand(self._oi, self._robot_container.flywheel_subsystem))
 
             if SmartDashboard.getBoolean("Feeder Test Enabled", False):
                 commands.append(FeederTestCommand(self._oi, self._robot_container.feeder_subsystem))
+
+            if SmartDashboard.getBoolean("Indexer Test Enabled", False):
+                commands.append(IndexerTestCommand(self._robot_container.indexer_subsystem, self._oi))
 
             if SmartDashboard.getBoolean("Intake Test Enabled", False):
                 commands.append(IntakeTestCommand(self._oi, self._robot_container.intake_subsystem))
