a663a2799a1741803ff9f4a79bde4ad6694713d4
somebody else <ethanfrancis577@gmail.com>
2026-02-14 11:23:44 -0500

    chore: refactor launcher

--- src/subsystems/launcher_subsystem.py
+++ src/subsystems/launcher_subsystem.py
@@ -39,7 +39,13 @@ class LauncherSubsystem(Subsystem):
         self.dist_array: np.ndarray
         self.flight_time_array: np.ndarray
         self.rpm_array: np.ndarray
+        self._turret_to_target: Translation2d
         self.stop()
+
+
+    @property
+    def turret_to_target(self) -> Translation2d:
+        return self._turret_to_target
 
     def _get_turret_position(self) -> Pose2d:
         robot_pose: Pose2d = self.drivetrain.get_pose()
@@ -56,12 +62,12 @@ class LauncherSubsystem(Subsystem):
         return ChassisSpeeds(robot_velocity.vx+turret_to_robot_velocity.X(), robot_velocity.vy+turret_to_robot_velocity.Y(), robot_velocity.omega)
 
 
-    def target_translation(self, target_translation: Translation2d) -> Translation2d:
+    def _calculate_turret_to_target_translation(self):
         turret_pose: Pose2d = self._get_turret_position()
         turret_translation: Translation2d = Translation2d(turret_pose.x, turret_pose.y)
         turret_rotation: Rotation2d = turret_pose.rotation()
 
-        difference: Translation2d = target_translation - turret_translation
+        difference: Translation2d = self._target - turret_translation
 
         flight_time: seconds = LauncherSubsystemConstants.LATENCY + np.interp(metersToInches(difference.norm()), self.dist_array, self.flight_time_array)
         turret_velocity: ChassisSpeeds = self._get_turret_velocity()
@@ -72,24 +78,26 @@ class LauncherSubsystem(Subsystem):
 
         difference.rotateBy(-turret_rotation)
 
-        return difference
+        self._turret_to_target = difference
 
     def periodic(self) -> None:
+        self._calculate_turret_to_target_translation()
         match self.state:
             case self.states.REST:
                 pass
             case self.states.TRACK:
-                self.turret.aim(self.target_translation(self._target))
+                self.turret.aim(self.turret_to_target)
             case self.states.PREPARE:
                 self._set_turret_and_flywheel()
                 if self.turret.at_target() and self.flywheel.is_at_speed():
                     self.state = self.states.FIRE
             case self.states.FIRE:
+                self._set_turret_and_flywheel()
                 self.indexer.set_power(IndexerSubsystemConstants.INDEX_POWER) if self.indexer != None else None
                 self.feeder.set_velocity(FeederSubsystemConstants.FEED_SPEED) if self.feeder != None else None
     
     def _set_turret_and_flywheel(self) -> None:
-        target_translation: Translation2d = self.target_translation(self._target)
+        target_translation: Translation2d = self.turret_to_target
         self.turret.aim(target_translation)
         self.flywheel.set_speed(
             SC_LauncherSpeed(
@@ -113,10 +121,9 @@ class LauncherSubsystem(Subsystem):
         self.state = self.states.TRACK
     
     def fire_at(self, target: Translation2d, target_type: Literal["hub", "feed"]):
-        self._target = target
-        self._target_type = target_type
-
-        self.state = self.states.PREPARE
+        self.aim_at(target, target_type)
+        if self.state != self.states.FIRE:
+            self.state = self.states.PREPARE
     
     def stop(self) -> None:
         self.feeder.set_power(0) if self.feeder != None else None
