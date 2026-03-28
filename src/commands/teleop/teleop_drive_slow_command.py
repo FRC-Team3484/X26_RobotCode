aa72a02875214eea15d2eb0b230d4e8b7039aadc
FRC3484_PRGDS <frc3484@gmail.com>
2026-03-28 10:58:13 -0400

    feat: Re-add the slew rate limit to the teleop drive slow command

--- src/commands/teleop/teleop_drive_slow_command.py
+++ src/commands/teleop/teleop_drive_slow_command.py
@@ -25,6 +25,10 @@ class TeleopDriveSlowCommand(Command):
 
         self._pivot_corner: Translation2d = Translation2d()
 
+        self._throttle_filter: SlewRateLimiter = SlewRateLimiter(TeleopDriveConstants.SLEW_FILTER_AMOUNT)
+        self._strafe_filter: SlewRateLimiter = SlewRateLimiter(TeleopDriveConstants.SLEW_FILTER_AMOUNT)
+        self._rotation_filter: SlewRateLimiter = SlewRateLimiter(TeleopDriveConstants.SLEW_FILTER_AMOUNT)
+
     def initialize(self) -> None:
         '''
         Drivetrain is field-centric but controls are driver-centric so we need to know the alliance color to know if conrols should be flipped.
@@ -39,7 +43,10 @@ class TeleopDriveSlowCommand(Command):
                     self._oi.get_jog_down(),
                     self._oi.get_jog_left()]
 
-            if self._oi.get_hold_mode():
+            if self._oi.get_reset_heading():
+                self._drivetrain.set_heading()
+
+            elif self._oi.get_hold_mode():
                 self._drivetrain.set_module_states((
                     SwerveModuleState(0.0, Rotation2d.fromDegrees(45.0)),
                     SwerveModuleState(0.0, Rotation2d.fromDegrees(135.0)),
@@ -60,28 +67,24 @@ class TeleopDriveSlowCommand(Command):
                     strafe += 1.0
 
                 self._drivetrain.drive_robotcentric(
-                    ChassisSpeeds(
-                        throttle * TeleopDriveConstants.JOG_SPEED,
-                        strafe * TeleopDriveConstants.JOG_SPEED,
-                        0.0
-                        ),
-                    True
-                    )
+                    ChassisSpeeds(throttle * TeleopDriveConstants.JOG_SPEED,
+                                strafe * TeleopDriveConstants.JOG_SPEED,
+                                    0.0),
+                    True)
             
             else:
+                self._throttle_filter.calculate(self._oi.get_throttle())
+                self._strafe_filter.calculate(self._oi.get_strafe())
+                self._rotation_filter.calculate(self._oi.get_rotation())
 
-                throttle: float = self._oi.get_throttle()
-                strafe: float = self._oi.get_strafe()
+                throttle: float = self._throttle_filter.lastValue()
+                strafe: float = self._strafe_filter.lastValue()
 
-                if DriverStation.getAlliance() == DriverStation.Alliance.kBlue:
+                if DriverStation.getAlliance() == DriverStation.Alliance.kRed:
                     throttle = -throttle
                     strafe = -strafe
 
-                self._drivetrain.drive(
-                    throttle * TeleopDriveConstants.LOW_SPEED, strafe * TeleopDriveConstants.LOW_SPEED,
-                    self._oi.get_rotation() * TeleopDriveConstants.LOW_SPEED,
-                    True
-                    )
+                self._drivetrain.drive(throttle * TeleopDriveConstants.SLOW_SPEED, strafe * TeleopDriveConstants.SLOW_SPEED, self._rotation_filter.lastValue() * TeleopDriveConstants.SLOW_ROTATION_SPEED, False)
 
     def end(self, interrupted: bool) -> None:
         self._drivetrain.stop_motors()
