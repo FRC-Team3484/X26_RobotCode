b2e694e80f11e7ce1aaf56a29ac7c36d8322e68e
Jakob <jakob@spurgat.net>
2026-04-02 10:00:36 -0400

    fix: added lookup table test

---
+++ tests/lookup_table_test.py
@@ -0,0 +1,14 @@
+import pytest
+from src.constants import FeedTargetSubsystemConstants
+
+def test_lookup_table():
+    hub_distances = FeedTargetSubsystemConstants.HUB_DISTANCES
+    hub_speeds = FeedTargetSubsystemConstants.HUB_RPM
+    hub_times = FeedTargetSubsystemConstants.HUB_FLIGHT_TIME
+
+    feed_distances = FeedTargetSubsystemConstants.FEED_DISTANCES
+    feed_speeds = FeedTargetSubsystemConstants.FEED_RPM
+    feed_times = FeedTargetSubsystemConstants.FEED_FLIGHT_TIME
+
+    assert len(hub_distances) == len(hub_speeds) == len(hub_times)
+    assert len(feed_distances) == len(feed_speeds) == len(feed_times)
